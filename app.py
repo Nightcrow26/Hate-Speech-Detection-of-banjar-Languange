230ffb0a0de56702ebafb73bd072f6e239346e53
Abdi <90949834+MuhammadNurAbdi@users.noreply.github.com>
2022-08-23 22:47:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main():
     prediction_labels = {'Ujaran Kebencian': 0, 'Bukan Ujaran Kebencian': 1}
     if st.button("Classify"):
         st.text("Original Text::\n{}".format(news_text))
-	tfidf = TfidfVectorizer()
+        tfidf = TfidfVectorizer()
         vect_text = tfidf.transform([news_text])
         with open('model.pkl', 'rb') as f:
              predictor = pickle.load(f)
