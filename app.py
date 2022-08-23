8b2316da072b51452cd895a696a6aa757afa5f54
Afrizal2609 <90843644+Afrizal2609@users.noreply.github.com>
2022-08-23 21:32:48 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ import pickle
 import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
-def get_key(val, my_dict):
-    for key, value in my_dict.items():
+def get_key(val,my_dict):
+    for key,value in my_dict.items():
         if val == value:
             return key
-
 
 def main():
     st.title("TF-IDF dan SVM Klasifikasi Ujaran Kebencian Bahasa Banjar")
@@ -17,11 +15,10 @@ def main():
         st.text("Original Text::\n{}".format(news_text))
         vect_text = TfidfVectorizer.transform([news_text])
         with open('model.pkl', 'rb') as f:
-            predictor = pickle.load(f)
+             predictor = pickle.load(f)
         prediction = predictor.predict(vect_text)
         final_result = get_key(prediction, prediction_labels)
         st.success("News Categorized as:: {}".format(final_result))
 
-
 if __name__ == '__main__':
-    main()
+	main()
