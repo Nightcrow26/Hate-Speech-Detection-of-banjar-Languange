1ad7231d8fefd0fc3c22cffc0c687cee8184094a
Afrizal2609 <90843644+Afrizal2609@users.noreply.github.com>
2023-08-01 09:22:09 +0700

    Update app.py
    change label

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def get_key(val, my_dict):
 def main():
     st.title("TF-IDF dan SVM Klasifikasi Ujaran Kebencian Bahasa Banjar")
     news_text = st.text_area("Enter News Here", "Type Here")
-    prediction_labels = {'Ujaran Kebencian': 0, 'Bukan Ujaran Kebencian': 1}
+    prediction_labels = {'Ujaran Kebencian': 1, 'Bukan Ujaran Kebencian': 0}
     if st.button("Classify"):
         st.text("Original Text::\n{}".format(news_text))
         vect_text = tfidf.transform([news_text])
